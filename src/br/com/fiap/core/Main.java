350d5e944e6e47fae197bac44a3602c69fb528a0
logonrm <logonrm@L2704MICRO20.fiap.com.br>
2017-05-31 22:58:45 -0300

    final aula

--- src/br/com/fiap/core/Main.java
+++ src/br/com/fiap/core/Main.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.fiap.entity.Forum;
+import br.com.fiap.entity.Usuario;
 
 public class Main {
 
@@ -17,10 +18,18 @@ public class Main {
 		ForumHelper fHelper = new ForumHelper(em);
 		
 		Forum f = new Forum();
-		f.setAssunto("JDBC");
-		f.setDescricao("Teste JDBC.");
+		f.setAssunto("TESTE");
+		f.setDescricao("Teste 1234");
 		
 		System.out.println(fHelper.salvar(f));
+		
+		Usuario u1 = new Usuario();
+		u1.setForum(f);
+		u1.setNome("Diogo");
+		u1.setEmail("diogobrito2000@yahoo.com.br");
+		
+		fHelper.adicionarUsuario(f.getId(), u1);
+		
 	}
 
 }
